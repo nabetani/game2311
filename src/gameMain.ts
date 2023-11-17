445477c665bca11de1480efb4519af344d304dcc
Nabetani <takenori@nabetani.sakura.ne.jp>
2023-11-17 22:26:48 +0900

    移動

--- src/gameMain.ts
+++ src/gameMain.ts
@@ -15,20 +15,27 @@ export class GameMain extends BaseScene {
       p1: "p1.png",
     });
   }
+
   create() {
     this.add.image(this.canX(0.5), this.canY(0.5), 'mainBG');
     this.p = [0, 1].map(e => this.add.sprite(200, 200 + e * 700, `p${e}`));
     this.p[0].setVisible(true);
+    const zone = this.add.zone(this.canX(0.5), this.canY(0.5), this.canX(1), this.canY(1));
+    zone.setInteractive();
+    zone.on('pointerup', () => { this.model.pointerup(); });
+    zone.on('pointerdown', () => { this.model.pointerdown(); });
   }
   pSprite(): Phaser.GameObjects.Sprite {
     return this.p[this.model.imageIx()];
   }
   update() {
+    this.model.notifyButtonState(0 != (this.input.mousePointer.buttons & 1));
     this.model.progress()
     for (let i = 0; i < this.p.length; i++) {
       this.p[i].setVisible(this.model.imageIx() == i);
     }
     const p = this.pSprite();
     p.setAngle(this.model.pAngle());
+    p.setPosition(this.model.pos.x, this.model.pos.y);
   }
 }
--- src/model.ts
+++ src/model.ts
@@ -1,9 +1,39 @@
+
 export class Model {
+  myAngle: number = 0;
+  deltaAngle: number = 0;
+  velo: number = 0;
+  poteVelo: number = 0;
+  pos: Phaser.Math.Vector2 = new Phaser.Math.Vector2(200, 200)
   progress() { }
   imageIx(): integer {
     return 0;
   }
   pAngle(): number {
-    return Math.random() * 360;
+    return this.myAngle;
+  }
+  notifyButtonState(down: boolean) {
+    if (down) {
+      this.deltaAngle = this.deltaAngle + Math.min(0.1, 1.0 / (0.1 + this.deltaAngle));
+      this.velo *= 0.8;
+      this.poteVelo += 0.2 + 1 / (this.poteVelo + 1);
+    } else {
+      this.velo *= 0.95;
+    }
+    this.myAngle += this.deltaAngle;
+    let t = (-90 + this.myAngle) * Math.PI / 180;
+    this.pos.x += this.velo * Math.cos(t)
+    this.pos.y += this.velo * Math.sin(t)
+  }
+  position(): Phaser.Math.Vector2 {
+    return this.pos;
+  }
+  pointerup() {
+    this.velo = this.poteVelo;
+    this.poteVelo = 0;
+    this.deltaAngle = 0;
+  }
+  pointerdown() {
+
   }
 }
